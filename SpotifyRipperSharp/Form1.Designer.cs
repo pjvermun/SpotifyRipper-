4b6b8bf04a2a84776d62bbf491f481c499f95d0b
Pieter-Jan Vermunicht <pjv1001@gmail.com>
2018-06-12 20:22:22 +0200

    first commit

---
+++ SpotifyRipperSharp/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SpotifyRipperSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.Location = new System.Drawing.Point(12, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(303, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Current song:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "START";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 141);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(69, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "STOP";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox3.Location = new System.Drawing.Point(12, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(87, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Number of songs:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(105, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(210, 145);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox2.Location = new System.Drawing.Point(12, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(87, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Folder size:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 210);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Spotify Ripper#";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SpotifyRipperSharp/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using CSCore;
+using CSCore;
+using CSCore.Codecs.WAV;
+using CSCore.MediaFoundation;
+using CSCore.SoundIn;
+using CSCore.Streams;
+using CSCore.CoreAudioAPI;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace SpotifyRipperSharp
+{
+    public partial class Form1 : Form
+    {
+        private WasapiCapture capture;
+        private WaveWriter writer;
+        private int songs;
+
+        List<String> _items = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            listBox1.DataSource = _items;
+            button5.Enabled = false;
+            songs = 0;
+        }
+
+        private void startRecording(string name)
+        {
+            capture = new WasapiLoopbackCapture();
+            capture.Initialize();
+            //var writer = MediaFoundationEncoder.CreateMP3Encoder(capture.WaveFormat, name + ".mp3", 320000);
+            writer = new WaveWriter("./recorded/"+ name + ".wav", capture.WaveFormat);
+            capture.DataAvailable += (s, capData) =>
+            {
+                writer.Write(capData.Data, capData.Offset, capData.ByteCount);
+            };
+            capture.Start();
+        }
+
+        private void stopRecording()
+        {
+            if (writer != null && capture != null)
+            {
+                capture.Stop();
+                writer.Dispose();
+                capture.Dispose();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            PowerShell test = PowerShell.Create().AddScript("Get-Process |where {$_.mainWindowTItle -and $_.name -eq 'Spotify'} | format-table mainwindowtitle -HideTableHeaders");
+            test.AddCommand("Out-String");
+            String previous = null;
+            String str = null;
+            Regex illegalInFileName = new Regex(@"[\\/:*?""<>|]");
+            while (true)
+            {
+                
+                if (backgroundWorker1.CancellationPending)
+                {
+                    stopRecording();
+                    return;
+                }
+                foreach (string stripped in test.Invoke<string>())
+                {
+                    str = stripped.Replace("\r\n", "");
+                    str = illegalInFileName.Replace(str, "");
+                    if (previous != str && str != "Spotify        ")
+                    {
+                        _items.Add(str);
+                        stopRecording();
+                        startRecording(str);
+                        backgroundWorker1.ReportProgress(12);
+                        songs++;
+                    }
+                    
+                    previous = str;
+                }
+                Thread.Sleep(100);
+                
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            textBox3.Text = songs.ToString();
+            listBox1.DataSource = null;
+            listBox1.DataSource = _items;
+            textBox1.Text = _items.Last();
+            string[] a = Directory.GetFiles("./recorded/", "*.*");
+            long b = 0;
+            foreach (string name in a)
+            {
+                FileInfo info = new FileInfo(name);
+                b += info.Length;
+            }
+            b /= 1000000;
+            textBox2.Text = b.ToString() + " MB";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+            button3.Enabled = false;
+            button5.Enabled = true;
+            songs = 0;
+            textBox3.Text = "0";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+            button3.Enabled = true;
+            button5.Enabled = false;
+        }
+    }
+}
